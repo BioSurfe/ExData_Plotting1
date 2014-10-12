60593db7af6da43fd8bb0ede026d8fbd0ed8973e
BioSurfe <jbvijayakumar@hotmail.com>
2014-10-12 14:50:33 +0530

    Update plot3.R
    fixed x

--- myplots/plot3.R
+++ myplots/plot3.R
@@ -11,26 +11,25 @@ ExFile <- rawFile[SubFile,]
 
 # Extract the date and time function into date and time format
 library(lubridate)
-ExFile$Date <- dmy(ExFile$Date)
-ExFile$Time <- hms(ExFile$Time)
+ExFile$ts <- dmy_hms(paste(ExFile$Date, ExFile$Time))
 
 #Plot 3
 with(ExFile,
-     plot(Sub_metering_1,ann = F, type = "n", xaxt = "n"),
-     plot(ub_metering_2,ann = F, type = "n", xaxt = "n"),
-     plot(Sub_metering_3,ann = F, type = "n", xaxt = "n"))
+     plot(ExFile$ts, Sub_metering_1, ann = F, type = "n"),
+     plot(ExFile$ts, Sub_metering_2, ann = F, type = "n"),
+     plot(ExFile$ts, Sub_metering_3, ann = F, type = "n"))
 
-lines(ExFile$Sub_metering_1, col = "black")
-lines(ExFile$Sub_metering_2, col = "red")
-lines(ExFile$Sub_metering_3, col = "blue")
-
-axis(side = 1, at =c(1,1440,2880), labels = c("Thu", "Fri", "Sat"))
+lines(ExFile$ts, ExFile$Sub_metering_1, col = "black")
+lines(ExFile$ts, ExFile$Sub_metering_2, col = "red")
+lines(ExFile$ts, ExFile$Sub_metering_3, col = "blue")
 
 title(ylab = "Energy sub metering", cex.lab = 0.8)
 
 legend("topright", lty = 1, col = c("black", "red", "blue"), 
-       legend = c("Sub_metering_1  ", "Sub_metering_2  ","Sub_metering_3  "),
-       )
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       x.intersp = 0.5, y.intersp = 0.5
+)
+
 # create PNG
 dev.copy(png, file = "plot3.png",width = 480, height = 480, units = "px")
 dev.off()
